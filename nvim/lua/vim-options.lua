30c64b54215e1618b2e10a4a7e8c24f6d6527a93
Shailesh Chauhan <164928149+Sh1lezh@users.noreply.github.com>
2025-06-15 18:12:35 +0530

    Add files via upload

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,20 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 2 
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.showmode = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 500
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.termguicolors = true
